371b839ebb250194130b2f34f4097571f3331f0a
insecurity22 <insecurity22@naver.com>
2026-04-26 20:21:36 +0900

    feat: solve 1 Hash problem (42578) and rename 42577 file date

---
+++ programmers/learn/challenges/algorithm_practice_kit/해시/20260426_42578.js
@@ -0,0 +1,28 @@
+function solution(clothes) { // [[의상의 이름, 의상의 종류], ...]
+    const typeCountMap = new Map();
+    for(const [, type] of clothes) {
+        typeCountMap.set(type, (typeCountMap.get(type) || 0) + 1);
+    }
+    
+    let combinations = 1; 
+    for(const count of typeCountMap.values()) {
+        const selectableOptions = count + 1;
+        combinations *= selectableOptions;
+    }
+    
+    return combinations - 1;
+}
+
+// 1. 안 입는 옷을 포함하기 위해 count + 1을 한다.
+// headgear(2개): yellow_hat, green_turban + X = 선택지 3개
+// eyewear(1개): blue_sunglasses + X = 선택지 2개
+
+// 2. 총 경우의 수 6개
+// (yellow_hat, blue_sunglasses)
+// (yellow_hat, 안 씀)
+// (green_turban, blue_sunglasses)
+// (green_turban, 안 씀)
+// (안 씀, blue_sunglasses)
+// (안 씀, 안 씀)
+
+// 3. 모두 안쓰는 경우는 제거
